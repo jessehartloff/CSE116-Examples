de9d629b7699f70d5fb3b36b3669d51485b27f77
jessehartloff <hartloff@buffalo.edu>
2021-02-05 13:29:01 -0500

    Add string splitter example

--- src/lo1_program_execution/scala/StringSplitter.scala
+++ src/lo1_program_execution/scala/StringSplitter.scala
@@ -27,11 +27,32 @@ object StringSplitter {
     trueCount / splits.length
   }
 
+  def accessSplitsExample(): Unit = {
+    val stringToSplit: String = "value1_value2_value3"
+
+    // Split the String on "_"
+    val splits: Array[String] = stringToSplit.split("_")
+
+    // Access the first value
+    val firstValue: String = splits(0)
+
+    // Access the second value
+    val secondValue: String = splits(1)
+
+    // Access the third value
+    val thirdValue: String = splits(2)
+
+    println(firstValue)
+    println(secondValue)
+    println(thirdValue)
+  }
+
   def main(args: Array[String]): Unit = {
     val testInput = "true;false;true;true;true"
     // expecting 0.8
     val percentTrue = computePercentTrue(testInput)
     println("Percentage true == " + percentTrue)
+    accessSplitsExample()
   }
 
 }
