e69bc3acf583e0ca9178dc1a883d7c43a5ef862d
jessehartloff <hartloff@buffalo.edu>
2021-02-24 14:36:57 -0500

    Add player example

--- src/lo2_oop/testing/TestPlayer.scala
+++ src/lo2_oop/testing/TestPlayer.scala
@@ -51,4 +51,41 @@ class TestPlayer extends FunSuite {
 
   }
 
+
+  test("Playing with references") {
+
+    // create player 1 who starts at the origin of the game world with 10 hp
+    val player1: Player = new Player(0.0, 0.0, 10)
+
+    // assign player2 the value of player1
+    // Since only references to objects are stored in variable, player2 is
+    // assigned the reference stored in player1
+    // The result: player1 and player2 refer to the same object on the heap. There is only 1 player
+    val player2: Player = player1
+
+    checkPlayerLocation(player1, 0.0, 0.0)
+    checkPlayerLocation(player2, 0.0, 0.0)
+
+    player2.move(3.0, 4.0)
+
+    // We only move player2. Since player1 and player2 refer to the same player, both appear to move
+    checkPlayerLocation(player1, 3.0, 4.0)
+    checkPlayerLocation(player2, 3.0, 4.0)
+
+    player2.attack(player1)
+    player2.attack(player1)
+
+    assert(player1.hp == 2)
+    assert(player1.conscious())
+    assert(player2.hp == 2)
+    assert(player2.conscious())
+
+    player2.attack(player1)
+
+    assert(!player1.conscious())
+    assert(!player2.conscious())
+
+  }
+
+
 }
