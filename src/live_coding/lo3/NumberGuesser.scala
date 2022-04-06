d78b6b118253f08bd7d4fbe595e91793b0707a25
jessehartloff <hartloff@buffalo.edu>
2022-04-06 16:54:30 -0400

    Add live recursion examples -histogram with tail recursion -number guessing game

--- src/live_coding/lo3/NumberGuesser.scala
+++ src/live_coding/lo3/NumberGuesser.scala
@@ -1,11 +1,28 @@
 package live_coding.lo3
 
+import scala.annotation.tailrec
+
 object NumberGuesser {
 
+  // f will return true if the hidden number is higher than the one you guessed
+  //  false if the number is lower of equal
   def guessTheNumber(lowerBound: Double,
                      upperBound: Double,
                      f: Double => Boolean): Double = {
-    0.0
+    val epsilon: Double = 0.01
+    val midpoint: Double = (upperBound + lowerBound) / 2.0
+    if(upperBound - lowerBound < epsilon){
+      midpoint
+    }else{
+      val higher: Boolean = f(midpoint)
+      val answer = if(higher){
+        guessTheNumber(midpoint, upperBound, f)
+      }else{
+        guessTheNumber(lowerBound, midpoint, f)
+      }
+      answer
+     }
+
   }
 
 }
