4a86773ae0097090d9ffa94e055170c870384cb8
Jesse Hartloff <hartloff@buffalo.edu>
2022-04-20 05:27:56 -0400

    Add LLNode memory diagram example; add missing call for binary tree example

---
+++ src/lo4_data_structures/linkedlist/LinkedListMemoryDiagramExample.scala
@@ -0,0 +1,18 @@
+package lo4_data_structures.linkedlist
+
+object LinkedListMemoryDiagramExample {
+
+  def main(args: Array[String]): Unit = {
+
+    var myList: LLNode[Int] = new LLNode[Int](1, null)
+    myList = new LLNode[Int](3, myList)
+    myList = new LLNode[Int](5, myList)
+
+    val theValue: Int = myList.next.next.value
+    println("theValue: " + theValue)
+
+    val listSize: Int = myList.size()
+    println("size: " + listSize)
+  }
+
+}
--- src/lo4_data_structures/trees/BinaryTreeTraversals.scala
+++ src/lo4_data_structures/trees/BinaryTreeTraversals.scala
@@ -48,6 +48,7 @@ object BinaryTreeTraversals {
   }
 
   def main(args: Array[String]): Unit = {
+    binaryTreeExamples()
   }
 
 
