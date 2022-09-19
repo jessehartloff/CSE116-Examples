9bf9ceed6a6263cdaca173eb8aa39b2e9dc696f2
Jesse Hartloff <hartloff@buffalo.edu>
2022-09-19 13:02:03 -0400

    String memory diagram

---
+++ src/lo1_program_execution/memory/StringExample.scala
@@ -0,0 +1,28 @@
+package lo1_program_execution.memory
+
+object StringExample {
+
+  def reverse(input: String): String = {
+    var result = ""
+    for(i <- input.indices){
+      result = input(i) + result
+    }
+    result
+  }
+
+  def modify(input: String): String = {
+    var result = ""
+    for(i <- 1 until input.length){
+      result += input(i)
+    }
+    result = reverse(result)
+    result
+  }
+
+  def main(args: Array[String]): Unit = {
+    var word: String = "dog"
+    word = modify(word)
+    println(word)
+  }
+
+}
